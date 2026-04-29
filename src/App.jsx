95e960c60d5d3a07a8770b1ecea340d1a6dfcb6c
byungkwon <ansqud14@gmail.com>
2026-04-29 21:46:31 +0900

    ~302 할일관리 앱 만들기 초기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App(){
   return (
     <div className="App">
-      <h2>헬로 리액트</h2>
+      <div>Header</div>
+      <div>Todo Editor</div>
+      <div>Todo List</div>
     </div>);
 }
 
