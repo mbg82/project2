bf888a0eb69d381695bf03f20967a8197861d1ac
byungkwon <ansqud14@gmail.com>
2026-05-02 21:35:43 +0900

    ~311p

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Header from "./component/Header";
+import TodoEditor from "./component/TodoEditor";
+import TodoList from "./component/TodoList";
 
 function App(){
   return (
     <div className="App">
-      <div>Header</div>
-      <div>Todo Editor</div>
-      <div>Todo List</div>
+      <Header />
+      <TodoEditor />
+      <TodoList />
     </div>);
 }
 
