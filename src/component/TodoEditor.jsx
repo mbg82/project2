e59a0c4ff47d3bcd4a13065462219a54a8221869
byungkwon <ansqud14@gmail.com>
2026-05-11 22:11:27 +0900

    ~340

--- src/component/TodoEditor.jsx
+++ src/component/TodoEditor.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useRef,useState } from "react";
 import "./TodoEditor.css";
 
 const TodoEditor=({ onCreate })=>{
     const [content,setContent]=useState("");
+    const inputRef=useRef();
     const onChangeContent=(e)=>{
         setContent(e.target.value);
     };
     const onSubmit = () => {
+        if (!content){
+            inputRef.current.focus();
+            return;
+        }
         onCreate(content);
+        setContent("");
+    };
+    const onKeyDown=(e)=>{
+        if (e.keyCode===13){
+            onSubmit();
+        }
     };
 
     return(
@@ -15,8 +26,10 @@ const TodoEditor=({ onCreate })=>{
             <h4>새로운 Todo 작성하기</h4>
             <div className="editor_wrapper">
                 <input 
+                 ref={inputRef}
                  value ={content}
                  onChange={onChangeContent}
+                 onKeyDown={onKeyDown}
                  placeholder="새로운 Todo..."
                 />        
                 <button onClick={onSubmit}>추가</button>
--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -1,13 +1,13 @@
 import "./TodoItem.css";
 
-const TodoItem=()=>{
+const TodoItem=({ id, content, isDone, createDate })=>{
     return(
         <div className="TodoItem">
             <div className="checkbox_col">
-                <input type="checkbox" />
+                <input checked={isDone} type="checkbox" />
             </div>
-            <div className="title_col">할 일</div>
-            <div className="date_col">{new Date().toLocaleDateString()}</div>
+            <div className="title_col">{content}</div>
+            <div className="date_col">{new Date(createDate).toLocaleDateString()}</div>
             <div className="btn_col">
                 <button>삭제</button>
             </div>
